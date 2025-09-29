5bee3be7a2523604205406109c742639391c8493
Azamandius13 <artemiy.kwork@gmail.com>
2025-09-29 21:14:15 +0300

    Вопрос на платформу

--- src/App.js
+++ src/App.js
@@ -1,9 +1,151 @@
-import './App.css';
+import './App.css'
+import Main from './components/Main'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-  <></>
-  );
+    <div className="wrapper">
+      <div className="container">
+        <main className="main">
+          <Navbar />
+          <Main />
+          <div className="main__sidebar sidebar">
+            <div className="sidebar__personal">
+              <p className="sidebar__personal-name">Sergey.Ivanov</p>
+              <div className="sidebar__icon">
+                <svg alt="logout">
+                  <use href="img/icon/sprite.svg#logout"></use>
+                </svg>
+              </div>
+            </div>
+            <div className="sidebar__block">
+              <div className="sidebar__list">
+                <div className="sidebar__item">
+                  <a className="sidebar__link" href="https://example.com">
+                    <img
+                      className="sidebar__img"
+                      src="img/playlist01.png"
+                      alt="day's playlist"
+                    />
+                  </a>
+                </div>
+                <div className="sidebar__item">
+                  <a className="sidebar__link" href="https://example.com">
+                    <img
+                      className="sidebar__img"
+                      src="img/playlist02.png"
+                      alt="day's playlist"
+                    />
+                  </a>
+                </div>
+                <div className="sidebar__item">
+                  <a className="sidebar__link" href="https://example.com">
+                    <img
+                      className="sidebar__img"
+                      src="img/playlist03.png"
+                      alt="day's playlist"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <div className="bar">
+          <div className="bar__content">
+            <div className="bar__player-progress"></div>
+            <div className="bar__player-block">
+              <div className="bar__player player">
+                <div className="player__controls">
+                  <div className="player__btn-prev">
+                    <svg className="player__btn-prev-svg" alt="prev">
+                      <use href="img/icon/sprite.svg#icon-prev"></use>
+                    </svg>
+                  </div>
+                  <div className="player__btn-play _btn">
+                    <svg className="player__btn-play-svg" alt="play">
+                      <use href="img/icon/sprite.svg#icon-play"></use>
+                    </svg>
+                  </div>
+                  <div className="player__btn-next">
+                    <svg className="player__btn-next-svg" alt="next">
+                      <use href="img/icon/sprite.svg#icon-next"></use>
+                    </svg>
+                  </div>
+                  <div className="player__btn-repeat _btn-icon">
+                    <svg className="player__btn-repeat-svg" alt="repeat">
+                      <use href="img/icon/sprite.svg#icon-repeat"></use>
+                    </svg>
+                  </div>
+                  <div className="player__btn-shuffle _btn-icon">
+                    <svg className="player__btn-shuffle-svg" alt="shuffle">
+                      <use href="img/icon/sprite.svg#icon-shuffle"></use>
+                    </svg>
+                  </div>
+                </div>
+
+                <div className="player__track-play track-play">
+                  <div className="track-play__contain">
+                    <div className="track-play__image">
+                      <svg className="track-play__svg" alt="music">
+                        <use href="img/icon/sprite.svg#icon-note"></use>
+                      </svg>
+                    </div>
+                    <div className="track-play__author">
+                      <a
+                        className="track-play__author-link"
+                        href="https://example.com"
+                      >
+                        Ты та...
+                      </a>
+                    </div>
+                    <div className="track-play__album">
+                      <a
+                        className="track-play__album-link"
+                        href="https://example.com"
+                      >
+                        Баста
+                      </a>
+                    </div>
+                  </div>
+
+                  <div className="track-play__like-dis">
+                    <div className="track-play__like _btn-icon">
+                      <svg className="track-play__like-svg" alt="like">
+                        <use href="img/icon/sprite.svg#icon-like"></use>
+                      </svg>
+                    </div>
+                    <div className="track-play__dislike _btn-icon">
+                      <svg className="track-play__dislike-svg" alt="dislike">
+                        <use href="img/icon/sprite.svg#icon-dislike"></use>
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bar__volume-block volume">
+                <div className="volume__content">
+                  <div className="volume__image">
+                    <svg className="volume__svg" alt="volume">
+                      <use href="img/icon/sprite.svg#icon-volume"></use>
+                    </svg>
+                  </div>
+                  <div className="volume__progress _btn">
+                    <input
+                      className="volume__progress-line _btn"
+                      type="range"
+                      name="range"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="footer"></footer>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
